18eb36dbb1901664873d30c1a37edff5737368ef
Lucas <lucasp.oliveira822@gmail.com>
2025-07-20 10:32:54 -0300

    puxando arquivos gerados

---
+++ src/main/java/com/educational/alura/OraculoDoSaber/OraculoDoSaberApplication.java
@@ -0,0 +1,13 @@
+package com.educational.alura.OraculoDoSaber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraculoDoSaberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OraculoDoSaberApplication.class, args);
+	}
+
+}
